511bed7dcfca4a040d96c9002984947ddaa76444
Tahi_ri96@itstep.edu.az <Tahi_ri96@itstep.edu.az>
2021-11-17 14:38:03 +0400

    Add project files.

---
+++ CancellationToken/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CancellationToken.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CancellationToken.ViewModels
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public RelayCommand MyProperty { get; set; }
+        public MainViewModel()
+        {
+
+        }
+    }
+}
